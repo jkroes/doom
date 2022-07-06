ed3bd08622c095862d9cd7b5c91941dd6c355012
Justin Kroes <jkroes14@ucsbalum.com>
2022-07-06 09:23:27 -0700

    Define command to find invalid chars for coding system

--- config.el
+++ config.el
@@ -118,3 +118,36 @@
     (insert (backtrace-to-string
              (backtrace-get-frames 'backtrace)))))
 (add-hook 'kill-emacs-hook 'save-backtrace)
+
+;; If you are getting prompts about bytes that don't work with the coding system
+;; and to save as a new encoding, use the following to find the bad characters.
+(defun find-next-unsafe-char (&optional coding-system)
+  "Find the next character in the buffer that cannot be encoded by
+coding-system. If coding-system is unspecified, default to the coding
+system that would be used to save this buffer. With prefix argument,
+prompt the user for a coding system."
+  (interactive "Zcoding-system: ")
+  (if (stringp coding-system) (setq coding-system (intern coding-system)))
+  (if coding-system nil
+    (setq coding-system
+          (or save-buffer-coding-system buffer-file-coding-system)))
+  (let ((found nil) (char nil) (csets nil) (safe nil))
+    (setq safe (coding-system-get coding-system 'safe-chars))
+    ;; some systems merely specify the charsets as ones they can encode:
+    (setq csets (coding-system-get coding-system 'safe-charsets))
+    (save-excursion
+      ;;(message "zoom to <")
+      (let ((end  (point-max))
+            (here (point    ))
+            (char  nil))
+        (while (and (< here end) (not found))
+          (setq char (char-after here))
+          (if (or (eq safe t)
+                  (< char ?\177)
+                  (and safe  (aref safe char))
+                  (and csets (memq (char-charset char) csets)))
+              nil ;; safe char, noop
+            (setq found (cons here char)))
+          (setq here (1+ here))) ))
+    (and found (goto-char (1+ (car found))))
+    found))
