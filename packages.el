542b0af0beb3ed55d323012e0a8625d1cdbbe2e6
Justin Kroes <jkroes14@ucsbalum.com>
2022-07-06 19:10:38 -0700

    Don't download Doom's snippets
    Hopefully this will allow me to keep the snippets module enabled
    so that lsp-mode works, without worrying about snippets
    triggering

--- packages.el
+++ packages.el
@@ -48,3 +48,6 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+
+;; Don't download any snippets
+(package! doom-snippets :ignore t)
