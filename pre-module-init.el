c9166ab8cab9b4713ea9084a5664e63d7fb147d7
Justin Kroes <jkroes14@ucsbalum.com>
2024-02-09 23:15:17 -0800

    stash

--- pre-module-init.el
+++ pre-module-init.el
@@ -4,6 +4,8 @@
 ;; files. Utility functions are loaded early in case they are
 ;; used by any module init.el.
 
+(load-doom-private "utility.el")
+
 (defconst IS-WSL (and (string-match "-[Mm]icrosoft" operating-system-release)
                       (eq system-type 'gnu/linux)))
 
@@ -14,5 +16,3 @@
       info
       vundo
       devdocs))
-
-(load-doom-private "utility.el")
